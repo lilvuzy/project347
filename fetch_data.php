759ff247840f7ce17c58bf7b1e04712ac512c809
lilvuzy <112009814+lilvuzy@users.noreply.github.com>
2023-04-09 14:52:38 -0400

    commits

---
+++ fetch_data.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "your_servername";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_dbname";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT date, value FROM your_table_name ORDER BY date ASC";
+$result = $conn->query($sql);
+$data = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
--- index.php
+++ index.php
@@ -1,14 +1,119 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-use Cowsayphp\Farm;
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
-header('Content-Type: text/plain');
+    <!-- Custom CSS -->
+    <style>
+        .footer {
+            background-color: #f8f9fa;
+            padding: 1rem 0;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
 
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
+    <title>Full Stack App</title>
+</head>
+<body>
 
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+<!-- Header -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">AppName</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tab 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tab 2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tab 3</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-cog"></i> Settings</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- End Header -->
+
+<!-- Main Content -->
+<main role="main" class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Graph Data</h1>
+            <div id="graph-container" style="width:100%; height:400px;">
+            <script>
+    $(document).ready(function() {
+        $.getJSON('fetch_data.php', function(data) {
+            let labels = [];
+            let values = [];
+
+            for (let i = 0; i < data.length; i++) {
+                labels.push(data[i].date);
+                values.push(data[i].value);
+            }
+
+            let ctx = document.getElementById('graph-container').getContext('2d');
+            let chart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Data over the course of a year',
+                        data: values,
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
+    });
+</script>
+
+        </div>
+        </div>
+    </div>
+</main>
+<!-- End Main Content -->
+
+<!-- Footer -->
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">&copy; 2023 AppName. All rights reserved.</span>
+    </div>
+</footer>
+<!-- End Footer -->
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<!-- Font Awesome for Settings icon -->
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+</body>
+</html>
