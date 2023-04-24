56147fc24e5b4cb306cdb816c3ab3465b76e703f
lilvuzy <helical_agape_0y@icloud.com>
2023-04-24 14:10:59 -0400

    updates

--- index.php
+++ index.php
@@ -15,9 +15,11 @@ if(!isset($_COOKIE["type"]))
  <head>
   <title>Yavuz Project</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script src="https://kit.fontawesome.com/1818889522.js" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
  </head>
  <body>
+    <main>
 
  <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
@@ -45,8 +47,8 @@ if(!isset($_COOKIE["type"]))
     <hr>
     <div class="dropdown">
       <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
+      <i class="fa-solid fa-user"></i>
+        <strong>User</strong>
       </a>
       <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2" style="">
         <li><a class="dropdown-item" href="#">New project...</a></li>
@@ -55,6 +57,8 @@ if(!isset($_COOKIE["type"]))
       </ul>
     </div>
   </div>
+
+</main>
 
 
   <br />
